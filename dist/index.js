feaaa098f8e68b21c2110bf7760b8743f21035d2
THEtheChad <chad.d.elliott@gmail.com>
2018-01-23 23:17:12 -0600

    removed dist folder and added prepublish hook to npm for building

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _parser = require('./parser');
-
-var _parser2 = _interopRequireDefault(_parser);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = _parser2.default;
